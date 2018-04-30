52672204ce0261243141aed3dab39a9399848815
kgorman <kgorman@me.com>
2018-04-30 16:50:54 -0500

    debugging, logging, etc

--- nema_gps_listener.py
+++ nema_gps_listener.py
@@ -6,15 +6,28 @@ logging.basicConfig(filename='udp.log', level=logging.DEBUG)
 
 
 def parse_nema(msg):
+    """ parse a nema coded message if we can """
     try:
         parsed_message = pynmea2.parse(msg)
     except:
-        parsed_message = msg
+        parsed_message = None
 
     return parsed_message
 
 
+def update_coordinates_file(payload):
+    """ write a file with lat/lon pairs """
+    try:
+        file = open("location.txt", "w")
+        file.write(payload)
+        file.close()
+    except:
+        print "unable to write location file"
+    return True
+
+
 def udp_server(host='0.0.0.0', port=8000):
+    """ a udb server """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -27,7 +40,19 @@ def udp_server(host='0.0.0.0', port=8000):
 
 def main():
     for data in udp_server():
-        logging.debug("%r" % (data,))
+        if data:
+
+            try:
+
+                thedate = "{} {}".format(data.datestamp.strftime("%Y-%m-%d"), data.timestamp.strftime("%H:%M:%S"))
+                output = "{} nema payload: {}".format(thedate, data)
+                logging.debug(output)
+
+                location = "{}, {}".format(data.lat, data.lon)
+                update_coordinates_file(location)
+
+            except Exception as e:
+                logging.debug(e)
 
 
 if __name__ == "__main__":
